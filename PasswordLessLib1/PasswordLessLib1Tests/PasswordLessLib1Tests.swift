2f3e912587eaa2d68e41db28288ca664e331fba1
Srini <srinit@meliosystems.com>
2022-08-01 10:15:20 +0530

    test cases

--- PasswordLessLib1/PasswordLessLib1Tests/PasswordLessLib1Tests.swift
+++ PasswordLessLib1/PasswordLessLib1Tests/PasswordLessLib1Tests.swift
@@ -10,6 +10,12 @@ import XCTest
 
 class PasswordLessLib1Tests: XCTestCase {
 
+    var PasswordLessLib: AccountManager!
+
+       override func setUp() {
+           PasswordLessLib = AccountManager()
+       }
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
