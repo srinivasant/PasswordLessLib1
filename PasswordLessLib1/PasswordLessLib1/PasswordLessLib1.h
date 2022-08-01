f93bef375e02629e74e051e660a53fdb8a9d1131
Srini <srinit@meliosystems.com>
2022-08-01 07:54:25 +0530

    Setup project

---
+++ PasswordLessLib1/PasswordLessLib1/PasswordLessLib1.h
@@ -0,0 +1,18 @@
+//
+//  PasswordLessLib1.h
+//  PasswordLessLib1
+//
+//  Created by Srinivasan T on 01/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PasswordLessLib1.
+FOUNDATION_EXPORT double PasswordLessLib1VersionNumber;
+
+//! Project version string for PasswordLessLib1.
+FOUNDATION_EXPORT const unsigned char PasswordLessLib1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PasswordLessLib1/PublicHeader.h>
+
+
